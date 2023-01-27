ccd34cfe3781492b055ae6375bb8808eee09596b
rizwanalvi <rizwan@aptechsite.net>
2023-01-27 17:52:24 +0500

    Initial commit

---
+++ Laravel/LA01/route-app/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    echo "ok";
+});
+Route::get('/{id}', function ($id) {
+    // return view('welcome');
+    echo "Required Parameter".$id;
+});
+
+Route::get('/test/{id?}', function ($id=2) {
+    // return view('welcome');
+    echo "Optional Parameter".$id;
+});
+
+Route::get('/{page}/{id}', function ($page,$id) {
+    // return view('welcome');
+    # /product/1
+    echo $page.':'.$id;
+});
+Route::get('/dum/{page}/{id?}', function ($page,$id=null) {
+    // return view('welcome');
+    # /product/1
+    echo $page.':';
+});
