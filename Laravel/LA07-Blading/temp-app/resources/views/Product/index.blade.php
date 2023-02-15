5f3cf994be9f5acce100ff5391accf9f86c0a198
rizwanalvi <rizwan@aptechsite.net>
2023-02-15 18:47:16 +0500

    RouteAndController

---
+++ Laravel/LA07-Blading/temp-app/resources/views/Product/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.master')
+@section('content')
+<h1>Product</h1>
+@endsection 
--- Laravel/LA07-Blading/temp-app/resources/views/layout/master.blade.php
+++ Laravel/LA07-Blading/temp-app/resources/views/layout/master.blade.php
@@ -218,7 +218,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="/product" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p class="text">Product</p>
             </a>
